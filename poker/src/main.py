7c5bd6dc25df657abebc39f2a5a390e0a1d1fe28
Vladimir Turov <aaaaaa2493@yandex.ru>
2018-02-26 02:44:52 +0300

    Moved run to separate file

--- poker/src/main.py
+++ poker/src/main.py
@@ -1,55 +1,5 @@
-
-from re import compile
-from datetime import datetime, timedelta
-from calendar import month_name
-from functools import lru_cache
-from os import listdir, mkdir, makedirs, remove
-from os.path import exists
-from shutil import rmtree
-from pickle import load, dump
-from random import shuffle, random, choice, uniform, randint
-from statistics import mean
-from threading import Thread, Lock
-from time import sleep
-from typing import List, Dict, Tuple, Optional, Iterator, Union
-from websocket import create_connection
-from json import loads, dumps
-from copy import deepcopy
-
-from special.debug import Debug
-from core.card import Card
-from holdem.cards_pair import CardsPair
-from holdem.holdem_poker import HoldemPoker as Poker, Hand
-from core.deck import Deck
-from learning.neural import Neural
-from holdem.base_play import BasePlay
-from special.settings import Settings, Mode
-
-
-
-
-class Run:
-
-    def __init__(self, mode: BasePlay.ModeType):
-
-        Play.Mode = mode
-        if mode == BasePlay.Mode.GameEngine:
-            # PlayManager.standings()
-            GameManager().run()
-
-        elif mode == BasePlay.Mode.Parse:
-            GameParser.parse_dir('pack1')
-            # game.save()
-            # game.convert()
-            # print(game)
-
-        elif mode == BasePlay.Mode.Evolution:
-            PlayManager.standings()
-            Evolution(100000, 9, 999, 10000, Blinds.Scheme.Rapid).run()
-
-        elif mode == BasePlay.Mode.Testing:
-            NeuralNetwork.PokerDecision.Bubble(100, 9).show()
-
+from special.settings import Mode
+from special.run import Run
 
 if __name__ == '__main__':
-    Run(BasePlay.Mode.Parse)
+    Run(Mode.Parse)
