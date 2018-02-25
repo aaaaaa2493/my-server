28107799a20b6db339b5dd40d169d28e2b3d8c3c
Vladimir Turov <aaaaaa2493@yandex.ru>
2018-02-25 23:18:40 +0300

    Move deck to core folder

--- poker/src/main.py
+++ poker/src/main.py
@@ -23,49 +23,7 @@ from special.debug import Debug
 from core.card import Card
 from holdem.cards_pair import CardsPair
 from holdem.holdem_poker import HoldemPoker as Poker, Hand
-
-
-class Deck:
-
-    def __init__(self):
-
-        self.cards: Card.Cards = Card.cards_52()
-        self.used: Card.Cards = []
-
-    def next(self) -> Card:
-
-        if not self.cards:
-            raise OverflowError('All 52 cards of the deck is already used')
-
-        card = self.cards[0]
-
-        self.cards[:1] = []
-        self.used += [card]
-
-        return card
-
-    def merge(self) -> None:
-
-        self.cards += self.used
-        self.used = []
-
-    def curr(self) -> Card:
-
-        return self.cards[0]
-
-    def shuffle(self) -> None:
-
-        self.merge()
-        shuffle(self.cards)
-
-    def skip(self, count: int = 1) -> None:
-
-        cards = self.cards[:count]
-        self.cards[:count] = []
-        self.used += cards
-
-    def __len__(self):
-        return len(self.cards)
+from core.deck import Deck
 
 
 class Neural:
