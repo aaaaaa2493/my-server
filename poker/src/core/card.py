82ed90a523c4686aa1e0be9eed45c3fe7222cfe5
Vladimir Turov <aaaaaa2493@yandex.ru>
2018-04-25 15:59:29 +0300

    Added separate classes for rank and suit

--- poker/src/core/card.py
+++ poker/src/core/card.py
@@ -1,29 +1,6 @@
 from typing import List, Dict
-from special.ordered_enum import OrderedEnum
-
-
-class Rank(OrderedEnum):
-    Ace = 14
-    King = 13
-    Queen = 12
-    Jack = 11
-    Ten = 10
-    Nine = 9
-    Eight = 8
-    Seven = 7
-    Six = 6
-    Five = 5
-    Four = 4
-    Three = 3
-    Two = 2
-    Invalid = 1
-
-
-class Suit(OrderedEnum):
-    Hearts = 1
-    Diamonds = 2
-    Clubs = 3
-    Spades = 4
+from core.rank import Rank
+from core.suit import Suit
 
 
 class Card:
@@ -35,45 +12,53 @@ class Card:
 
     Ranks: str = '23456789TJQKA'
 
-    ToRank: Dict[Rank, str] = {Rank.Two: 'two',
-                               Rank.Three: 'three',
-                               Rank.Four: 'four',
-                               Rank.Five: 'five',
-                               Rank.Six: 'six',
-                               Rank.Seven: 'seven',
-                               Rank.Eight: 'eight',
-                               Rank.Nine: 'nine',
-                               Rank.Ten: 'ten',
-                               Rank.Jack: 'jack',
-                               Rank.Queen: 'queen',
-                               Rank.King: 'king',
-                               Rank.Ace: 'ace'}
+    ToRank: Dict[Rank, str] = {
+        Rank.Two: 'two',
+        Rank.Three: 'three',
+        Rank.Four: 'four',
+        Rank.Five: 'five',
+        Rank.Six: 'six',
+        Rank.Seven: 'seven',
+        Rank.Eight: 'eight',
+        Rank.Nine: 'nine',
+        Rank.Ten: 'ten',
+        Rank.Jack: 'jack',
+        Rank.Queen: 'queen',
+        Rank.King: 'king',
+        Rank.Ace: 'ace'
+    }
 
-    FromRank: Dict[str, Rank] = {'2': Rank.Two,
-                                 '3': Rank.Three,
-                                 '4': Rank.Four,
-                                 '5': Rank.Five,
-                                 '6': Rank.Six,
-                                 '7': Rank.Seven,
-                                 '8': Rank.Eight,
-                                 '9': Rank.Nine,
-                                 'T': Rank.Ten,
-                                 'J': Rank.Jack,
-                                 'Q': Rank.Queen,
-                                 'K': Rank.King,
-                                 'A': Rank.Ace}
+    FromRank: Dict[str, Rank] = {
+        '2': Rank.Two,
+        '3': Rank.Three,
+        '4': Rank.Four,
+        '5': Rank.Five,
+        '6': Rank.Six,
+        '7': Rank.Seven,
+        '8': Rank.Eight,
+        '9': Rank.Nine,
+        'T': Rank.Ten,
+        'J': Rank.Jack,
+        'Q': Rank.Queen,
+        'K': Rank.King,
+        'A': Rank.Ace
+    }
 
     Suits: str = 'HDCS'
 
-    ToSuit: Dict[Suit, str] = {Suit.Hearts: 'hearts',
-                               Suit.Diamonds: 'diamonds',
-                               Suit.Clubs: 'clubs',
-                               Suit.Spades: 'spades'}
+    ToSuit: Dict[Suit, str] = {
+        Suit.Hearts: 'hearts',
+        Suit.Diamonds: 'diamonds',
+        Suit.Clubs: 'clubs',
+        Suit.Spades: 'spades'
+    }
 
-    FromSuit: Dict[str, Suit] = {'H': Suit.Hearts,
-                                 'D': Suit.Diamonds,
-                                 'C': Suit.Clubs,
-                                 'S': Suit.Spades}
+    FromSuit: Dict[str, Suit] = {
+        'H': Suit.Hearts,
+        'D': Suit.Diamonds,
+        'C': Suit.Clubs,
+        'S': Suit.Spades
+    }
 
     @staticmethod
     def cards_52() -> Cards:
---
+++ poker/src/core/suit.py
@@ -0,0 +1,8 @@
+from special.ordered_enum import OrderedEnum
+
+
+class Suit(OrderedEnum):
+    Hearts = 1
+    Diamonds = 2
+    Clubs = 3
+    Spades = 4
