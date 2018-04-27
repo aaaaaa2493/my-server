17ce8c1cff5d270f6fbd7bc7299d26abf34b36fc
Vladimir Turov <aaaaaa2493@yandex.ru>
2018-04-27 18:31:03 +0300

    Fix path

--- poker/lib/setup.py
+++ poker/lib/setup.py
@@ -1,4 +1,4 @@
 from sys import executable
 from os.path import dirname
 from shutil import copyfile
-copyfile(f'poker/lib/card.py', f'{dirname(executable)}/site-packages/pokereval/card.py')
+copyfile(f'poker/lib/card.py', f'{dirname(executable)}/../lib/python3.6/site-packages/pokereval/card.py')
