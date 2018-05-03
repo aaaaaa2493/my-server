cd1fc2ffd6cea4829aa0111d360880ff69f669b0
Vladimir Turov <aaaaaa2493@yandex.ru>
2018-05-03 23:26:25 +0300

    Improved testing for loop imports

--- poker/src/unit_tests/tests/importing.py
+++ poker/src/unit_tests/tests/importing.py
@@ -1,14 +1,11 @@
 from unittest import TestCase
-from os.path import dirname
 from importlib import import_module
 from unit_tests.testing import UnitTesting
 
 
 class ImportingTest(TestCase):
     def test_import_loop(self):
-        dr = dirname(__file__).replace('\\', '/')
-        dr = dr[:dr.find('/poker/src') + 10]
-        modules = UnitTesting.find_modules(dr)
+        modules = UnitTesting.find_modules('src')
         with self.assertRaises(ImportError):
             import_module('123')
         for module in modules:
