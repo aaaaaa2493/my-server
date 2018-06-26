81cb3bc3bb07f3a8f63ac1a9513830c644be85a2
aaaaaa2493 <aaaaaa2493@yandex.ru>
2018-06-26 17:46:33 +0300

    Added --profile key to script

--- poker/profile.cmd
+++ poker/profile.cmd
@@ -1,2 +1,2 @@
-python -m cProfile -o profile_results.prof .\src\main.py --evolution-tests
+python -m cProfile -o profile_results.prof .\src\main.py --evolution-tests --profile
 gprof2dot -f pstats profile_results.prof | dot -Tpng -o profile_results.png
